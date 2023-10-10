6ba062964b66a54771ec0cadb2ea8b740163f926
Manteiga <rafael.manteiga@hotmail.com>
2023-10-10 00:51:11 -0300

    Add files via upload

--- P1n/src/ValidaData.java
+++ P1n/src/ValidaData.java
@@ -1,3 +1,25 @@
 public class ValidaData {
-    
+    static boolean isDia(int dia){
+        return false;
+    }
+
+    static boolean isMes(int mes){
+        return false;
+    }
+
+    static boolean isAno(int ano){
+        return false;
+    }
+
+    static boolean isDia(String dia){
+        return false;
+    }
+
+    static boolean isMes(String mes){
+        return false;
+    }
+
+    static boolean isAno(String ano){
+        return false;
+    }
 }
