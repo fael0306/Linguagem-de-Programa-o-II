f3cbe23404af846037f7c7df92b68cb46a8954c6
Manteiga <rafael.manteiga@hotmail.com>
2023-11-06 23:43:31 -0300

    Update MinhaListaOrdenavel.java

--- P2n/src/MinhaListaOrdenavel.java
+++ P2n/src/MinhaListaOrdenavel.java
@@ -2,77 +2,77 @@ import java.util.*;
 
 public class MinhaListaOrdenavel {
 
-	private ArrayList<PessoaIMC> listapessoa = new ArrayList<PessoaIMC>();
+  private ArrayList < PessoaIMC > listapessoa = new ArrayList < PessoaIMC > ();
 
-	public void add(PessoaIMC p) {
-		listapessoa.add(p);
-	}
+  public void add(PessoaIMC p) {
+    listapessoa.add(p);
+  }
 
-	public PessoaIMC get(int index) {
-		return listapessoa.get(index);
-	}
+  public PessoaIMC get(int index) {
+    return listapessoa.get(index);
+  }
 
-	public Comparator<PessoaIMC> pesoC = new Comparator<PessoaIMC>() {
-        public int compare(PessoaIMC p1, PessoaIMC p2) {
-            return (int) Math.round(p2.getPeso() - p1.getPeso());
-        }
-    };
+  public Comparator < PessoaIMC > pesoC = new Comparator < PessoaIMC > () {
+    public int compare(PessoaIMC p1, PessoaIMC p2) {
+      return (int) Math.round(p2.getPeso() - p1.getPeso());
+    }
+  };
 
-	public Comparator<PessoaIMC> nomeAZC = new Comparator<PessoaIMC>() {
-        public int compare(PessoaIMC p1, PessoaIMC p2) {
-            return p1.getNome().compareTo(p2.getNome());
-        }
-    };
+  public Comparator < PessoaIMC > nomeAZC = new Comparator < PessoaIMC > () {
+    public int compare(PessoaIMC p1, PessoaIMC p2) {
+      return p1.getNome().compareTo(p2.getNome());
+    }
+  };
 
-	public Comparator<PessoaIMC> nomeZAC = new Comparator<PessoaIMC>() {
-        public int compare(PessoaIMC p1, PessoaIMC p2) {
-            return p2.getNome().compareTo(p1.getNome());
-        }
-    };
+  public Comparator < PessoaIMC > nomeZAC = new Comparator < PessoaIMC > () {
+    public int compare(PessoaIMC p1, PessoaIMC p2) {
+      return p2.getNome().compareTo(p1.getNome());
+    }
+  };
 
-	public Comparator<PessoaIMC> alturaC = new Comparator<PessoaIMC>() {
-        public int compare(PessoaIMC p1, PessoaIMC p2) {
-            return (int) Math.round(p2.getAltura() - p1.getAltura());
-        }
-    };
+  public Comparator < PessoaIMC > alturaC = new Comparator < PessoaIMC > () {
+    public int compare(PessoaIMC p1, PessoaIMC p2) {
+      return (int) Math.round(p2.getAltura() - p1.getAltura());
+    }
+  };
 
-	public Comparator<PessoaIMC> IMCC = new Comparator<PessoaIMC>() {
-        public int compare(PessoaIMC p1, PessoaIMC p2) {
-            return (int) Math.round(p2.calculaIMC()-p1.calculaIMC());
-        }
-    };
+  public Comparator < PessoaIMC > IMCC = new Comparator < PessoaIMC > () {
+    public int compare(PessoaIMC p1, PessoaIMC p2) {
+      return (int) Math.round(p2.calculaIMC() - p1.calculaIMC());
+    }
+  };
 
-	public ArrayList<PessoaIMC> ordena(final int c) {
+  public ArrayList < PessoaIMC > ordena(final int c) {
 
-		Comparator<PessoaIMC> comparador = null;
+    Comparator < PessoaIMC > comparador = null;
 
-		switch (c) {
-            case 1:
-                comparador = nomeAZC;
-                break;
-            case 2:
-                comparador = nomeZAC;
-                break;
-            case 3:
-                comparador = pesoC;
-                break;
-			case 4:
-				comparador = alturaC;
-				break;
-			case 5:
-				comparador = IMCC;
-				break;
-        }
+    switch (c) {
+    case 1:
+      comparador = nomeAZC;
+      break;
+    case 2:
+      comparador = nomeZAC;
+      break;
+    case 3:
+      comparador = pesoC;
+      break;
+    case 4:
+      comparador = alturaC;
+      break;
+    case 5:
+      comparador = IMCC;
+      break;
+    }
 
-		if(comparador!=null){
-			Collections.sort(listapessoa,comparador);
-			return listapessoa;
-		}
-		// Tratar este erro posteriormente
-		else{
-			System.out.println("Você digitou uma constante inválida. Programa encerrado.");
-            System.exit(0);
-            return null;
-		}
-	}
+    if (comparador != null) {
+      Collections.sort(listapessoa, comparador);
+      return listapessoa;
+    }
+    // Tratar este erro posteriormente
+    else {
+      System.out.println("Você digitou uma constante inválida. Programa encerrado.");
+      System.exit(0);
+      return null;
+    }
+  }
 }
