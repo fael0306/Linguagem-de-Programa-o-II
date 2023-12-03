3813edd7f8c2a6cda19ba6ba9399bd1a8b57eca2
Manteiga <rafael.manteiga@hotmail.com>
2023-12-03 05:52:18 -0300

    Add files via upload

--- P3n/src/lp2g21/biblioteca/Main.java
+++ P3n/src/lp2g21/biblioteca/Main.java
@@ -5,46 +5,46 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static void manutencao(){
+  private static void manutencao() {
 
-        Biblioteca biblioteca;
+    Biblioteca biblioteca;
 
-        Scanner op = new Scanner(System.in);
-        System.out.println("\n1 - Iniciar uma Biblioteca do zero\n2 - Carregar os arquivos da biblioteca");
-        int escolha = op.nextInt();
+    Scanner op = new Scanner(System.in);
+    System.out.println("\n1 - Iniciar uma Biblioteca do zero\n2 - Carregar os arquivos da biblioteca");
+    int escolha = op.nextInt();
 
-        switch (escolha) {
-            case 1:
-                Hashtable<Integer, Usuario> usuarios = new Hashtable<>();
-                Hashtable<String, Livro> livros = new Hashtable<>();
-                biblioteca = new Biblioteca(usuarios, livros);
-                biblioteca.salvaArquivo(usuarios, "usuarios");
-                biblioteca.salvaArquivo(livros, "livros");
-                System.out.println("\nBiblioteca criada com sucesso.");
-                break;
-            case 2:
-                biblioteca = new Biblioteca("usuarios","livros");
-                break;
-            default:
-                System.out.println("\nEscolha uma opção válida.");
-                return;
-        }
-
-    }
-    
-    private static void cadastro(){
-        
+    switch (escolha) {
+    case 1:
+      Hashtable < Integer, Usuario > usuarios = new Hashtable < > ();
+      Hashtable < String, Livro > livros = new Hashtable < > ();
+      biblioteca = new Biblioteca(usuarios, livros);
+      biblioteca.salvaArquivo(usuarios, "usuarios");
+      biblioteca.salvaArquivo(livros, "livros");
+      System.out.println("\nBiblioteca criada com sucesso.");
+      break;
+    case 2:
+      biblioteca = new Biblioteca("usuarios", "livros");
+      break;
+    default:
+      System.out.println("\nEscolha uma opção válida.");
+      return;
     }
 
-    private static void emprestimo(){
+  }
 
-    }
+  private static void cadastro() {
 
-    private static void gerarRelatorio(){
+  }
 
-    }
+  private static void emprestimo() {
 
-    public static void main(String[] args) {
-        
-    }    
+  }
+
+  private static void gerarRelatorio() {
+
+  }
+
+  public static void main(String[] args) {
+    manutencao();
+  }
 }
